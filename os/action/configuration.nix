cead7c9680b32dc4e3de78e26d430b6386b4b0b9
Shogo Takata <peshogo@gmail.com>
2023-09-19 05:39:12 +0900

    2023-09-19T05:39+09:00

--- os/action/configuration.nix
+++ os/action/configuration.nix
@@ -16,7 +16,7 @@
   #boot.loader.grub.useOSProber = true;
   #boot.loader.grub.device = "nodev";
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.binfmt.emulatedSystems = [ "aarch64-linux" "riscv64-linux" ];
