b30d9674004e7ee88aca9f3d4d74203e31dfbdf7
Shogo Takata <peshogo@gmail.com>
2024-02-20 13:43:09 +0900

    2024-02-20T13:43+09:00

--- os/action/configuration.nix
+++ os/action/configuration.nix
@@ -221,7 +221,7 @@
   ];
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 8080 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
