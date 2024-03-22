191216c5f43ce827c88f8361aa88a3126034475d
Shogo Takata <peshogo@gmail.com>
2024-03-22 15:18:01 +0900

    latest kernel

--- machines/beast/configuration.nix
+++ machines/beast/configuration.nix
@@ -11,6 +11,8 @@
       ./hardware-configuration.nix
     ];
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.consoleMode = "max";
