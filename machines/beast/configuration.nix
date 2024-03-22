e0f4db19690e215bf5fe7d9183233a84662d995d
Shogo Takata <peshogo@gmail.com>
2024-03-22 15:14:47 +0900

    update systemd-boot screen size

--- machines/beast/configuration.nix
+++ machines/beast/configuration.nix
@@ -13,6 +13,7 @@
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.consoleMode = "max";
   #boot.loader.efi.canTouchEfiVariables = true;
   boot.binfmt.emulatedSystems = [ "aarch64-linux" "riscv64-linux" ];
 
