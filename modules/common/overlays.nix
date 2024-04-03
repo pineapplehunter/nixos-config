a3ec6f80547e0a671bb7183f8655c6a93afa3169
Shogo Takata <peshogo@gmail.com>
2024-04-04 00:51:11 +0900

    2024-04-04T00:51+09:00

--- modules/common/overlays.nix
+++ modules/common/overlays.nix
@@ -89,6 +89,16 @@ with inputs; {
           })
         ];
       });
+
+      gnome = super.gnome // {
+        gnome-settings-daemon = super.gnome.gnome-settings-daemon.overrideAttrs (old: {
+          # I don't need sleep notifications!
+          postPatch = (old.postPatch or "") + ''
+            substituteInPlace plugins/power/gsd-power-manager.c \
+              --replace-fail "show_sleep_warning (manager);" "if(0) show_sleep_warning (manager);"
+          '';
+        });
+      };
     })
   ];
 }
