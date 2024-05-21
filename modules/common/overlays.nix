5290c642e1a7c83bafd261381a343296924ef292
Shogo Takata <peshogo@gmail.com>
2024-05-21 16:51:36 +0900

    2024-05-21T16:51+09:00

--- modules/common/overlays.nix
+++ modules/common/overlays.nix
@@ -40,6 +40,9 @@ with inputs; {
         });
       };
 
+      ibus-engines = super.ibus-engines // {
+        mozc = inputs.nixpkgs-pineapplehunter.legacyPackages.x86_64-linux.ibus-engines.mozc;
+      };
       # ibus-engines = super.ibus-engines // {
       #   mozc = super.ibus-engines.mozc.overrideAttrs (old: rec {
       #     src = super.fetchFromGitHub {
