24a146da06a23c61a3f79949a36a28a1cac8f27c
Shogo Takata <peshogo@gmail.com>
2024-06-13 03:01:57 +0900

    2024-06-13T03:01+09:00

--- packages/typst-thumbnailer/default.nix
+++ packages/typst-thumbnailer/default.nix
@@ -4,25 +4,23 @@
 , typst
 , lib
 }:
-let
-  script = writeShellScript "typst2png" ''
+runCommand "typst-thumbnailer"
+{
+  script = ''
     TMP=/tmp/typst2png/$(basename $1)
     mkdir -p $TMP
     ${lib.getExe typst} compile --format png $1 $TMP/output{n}.png
     cp $TMP/output1.png $2
     rm -r $TMP
   '';
-  thumbnailer = writeTextFile {
-    name = "typst.thumbnailer";
-    text = ''
-      [Thumbnailer Entry]
-      Exec=${placeholder "out"}/bin/typst2png %i %o
-      MimeType=text/x-typst
-    '';
-  };
-in
-runCommand "typst-thumbnailer" { } ''
+  thumbnailer = ''
+    [Thumbnailer Entry]
+    Exec=${placeholder "out"}/bin/typst2png %i %o
+    MimeType=text/x-typst
+  '';
+  passAsFile = [ "script" "thumbnailer" ];
+} ''
   mkdir -pv $out/{share/thumbnailers,bin}
-  cp -v ${script} $out/bin/typst2png
-  cp -v ${thumbnailer} $out/share/thumbnailers/typst.thumbnailer
+  cp -v $scriptPath $out/bin/typst2png
+  cp -v $thumbnailerPath $out/share/thumbnailers/typst.thumbnailer
 ''
