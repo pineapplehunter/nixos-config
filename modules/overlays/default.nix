22265473b9ee4a7e977d6c321e7236504376d01f
Shogo Takata <takata.shogo@riken.jp>
2024-06-17 12:58:11 +0900

    2024-06-17T12:58+09:00

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -3,15 +3,28 @@ let
   overlayCombined = final: prev:
     let
       callOverlay = file: import file { inherit final prev inputs; };
-      genOverlays = overlays: (lib.attrsets.mergeAttrsList (map callOverlay overlays));
+      genOverlays = overlays: lib.attrsets.mergeAttrsList (map callOverlay overlays);
+      removeDesktopEntry = package: {
+        ${package} = final.symlinkJoin rec {
+          inherit (prev.${package}) pname version;
+          name = "${pname}-no-dekstop-${version}";
+          paths = [ prev.${package} ];
+          postBuild = ''
+            rm -rfv $out/share/applications
+          '';
+        };
+      };
+      genRemoveDesktopEntries = packages: lib.attrsets.mergeAttrsList (map removeDesktopEntry packages);
     in
     (genOverlays [
       ./nixos-artwork-wallpaper.nix
-      ./blender.nix
+      # ./blender.nix
       ./curl-http3.nix
       ./flatpak.nix
       ./android-studio.nix
-    ]) // {
+    ])
+    // (genRemoveDesktopEntries [ "julia" "btop" "htop" "helix"  ])
+    // {
       gnome = prev.gnome // (genOverlays [
         ./gnome-settings-daemon.nix
       ]);
