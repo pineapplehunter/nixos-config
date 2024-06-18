74c22a295dbc6f397e7a240f2f5d3b594febafe7
Shogo Takata <peshogo@gmail.com>
2024-06-18 14:07:31 +0900

    2024-06-18T14:07+09:00

--- home/common/default.nix
+++ home/common/default.nix
@@ -5,34 +5,15 @@
     helix = {
       enable = true;
       defaultEditor = true;
-      languages = {
-        language-server.nixd.command = lib.getExe pkgs.nixd;
-        language = [{
-          name = "nix";
-          auto-format = true;
-          language-servers = [ "nixd" "nil" ];
-        }];
+      languages = import ./helix-languages.nix {
+        inherit lib;
+        inherit (pkgs) nixd;
       };
     };
 
     zellij = {
       enable = true;
-      settings = {
-        theme = "light";
-        themes.light = {
-          fg = "#DCD7BA";
-          bg = "#1F1F28";
-          red = "#C34043";
-          green = "#76946A";
-          yellow = "#FF9E3B";
-          blue = "#0000FF";
-          magenta = "#957FB8";
-          orange = "#FFA066";
-          cyan = "#7FB4CA";
-          black = "#16161D";
-          white = "#DCD7BA";
-        };
-      };
+      settings = import ./zellij-config.nix;
     };
 
     bat = {
@@ -46,44 +27,7 @@
 
     alacritty = {
       enable = true;
-      settings = {
-        colors = {
-          primary = {
-            background = "#ffffff";
-            foreground = "#24292f";
-          };
-          normal = {
-            black = "#24292e";
-            red = "#d73a49";
-            green = "#28a745";
-            yellow = "#dbab09";
-            blue = "#0366d6";
-            magenta = "#5a32a3";
-            cyan = "#0598bc";
-            white = "#6a737d";
-          };
-        };
-        bright = {
-          black = "#959da5";
-          red = "#cb2431";
-          green = "#22863a";
-          yellow = "#b08800";
-          blue = "#005cc5";
-          magenta = "#5a32a3";
-          cyan = "#3192aa";
-          white = "#d1d5da";
-        };
-        colors.indexed_colors = [
-          {
-            index = 16;
-            color = "#d18616";
-          }
-          {
-            index = 17;
-            color = "#cb2431";
-          }
-        ];
-      };
+      settings = import ./alacritty-config.nix;
     };
 
     btop = {
---
+++ home/common/helix-languages.nix
@@ -0,0 +1,11 @@
+{ lib
+, nixd
+}:
+{
+  language-server.nixd.command = lib.getExe nixd;
+  language = [{
+    name = "nix";
+    auto-format = true;
+    language-servers = [ "nixd" "nil" ];
+  }];
+}
