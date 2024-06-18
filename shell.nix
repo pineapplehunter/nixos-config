051426d5c74598bfbe6f1b028c489187efaaac84
Shogo Takata <peshogo@gmail.com>
2024-06-18 16:02:50 +0900

    add home scripts

--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@
 , nvd
 , nixos-rebuild
 , mkShellNoCC
+, home-manager
 }:
 
 let
@@ -49,18 +50,61 @@ let
     nix flake update
     ${getExe switch-script} "$@"
   '';
+  build-home-script = writeShellScriptBin "build-home" ''
+    ${getExe home-manager} build --flake ".#$USER" "$@"
+    exit $?
+  '';
+  diff-home-script = writeShellScriptBin "diff-home" ''
+    set -e
+    ${getExe build-home-script} "$@"
+    if [ $(readlink -f ./result) = $(readlink -f $HOME/.local/state/nix/profiles/home-manager) ]; then
+      echo All packges up to date!
+      exit 1
+    fi
+    ${getExe nvd} diff $HOME/.local/state/nix/profiles/home-manager ./result
+  '';
+  switch-home-script = writeShellScriptBin "switch-home" ''
+    set -e
+    ${getExe diff-home-script} "$@"
+    function yes_or_no {
+        while true; do
+            read -p "$* [y/n]: " yn
+            case $yn in
+                [Yy]*) return 0  ;;
+                [Nn]*) echo "Aborted" ; return 1 ;;
+            esac
+        done
+    }
+    yes_or_no "do you want to commit and update?"
+    echo starting switch
+    git commit -am "$(date -Iminutes)-home" || true
+    ${getExe home-manager} switch --flake ".#$USER" "$@"
+  '';
+  update-home-script = writeShellScriptBin "update-home" ''
+    git pull
+    nix flake update
+    ${getExe switch-home-script} "$@"
+  '';
 in
 mkShellNoCC {
+  name = "nixos-config";
   packages = [
     build-script
     switch-script
     diff-script
     update-script
     boot-script
+
+    build-home-script
+    diff-home-script
+    switch-home-script
+    update-home-script
+
     nvd
     nix-output-monitor
   ];
   shellHook = ''
     export HOST=`hostname`
+    export USER=`whoami`
   '';
 }
