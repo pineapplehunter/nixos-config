a4423c0212b2c9a83968fb8c57e1999b60f9bcbf
shogo <s196643z@st.go.tuat.ac.jp>
2024-06-19 10:04:35 +0900

    2024-06-19T10:04+09:00-home

--- shell.nix
+++ shell.nix
@@ -50,22 +50,22 @@ let
     nix flake update
     ${getExe switch-script} "$@"
   '';
-  build-home-script = writeShellScriptBin "build-home" ''
+  home-build-script = writeShellScriptBin "home-build" ''
     ${getExe home-manager} build --flake ".#$USER" "$@"
     exit $?
   '';
-  diff-home-script = writeShellScriptBin "diff-home" ''
+  home-diff-script = writeShellScriptBin "home-diff" ''
     set -e
-    ${getExe build-home-script} "$@"
+    ${getExe home-build-script} "$@"
     if [ $(readlink -f ./result) = $(readlink -f $HOME/.local/state/nix/profiles/home-manager) ]; then
       echo All packges up to date!
       exit 1
     fi
     ${getExe nvd} diff $HOME/.local/state/nix/profiles/home-manager ./result
   '';
-  switch-home-script = writeShellScriptBin "switch-home" ''
+  home-switch-script = writeShellScriptBin "home-switch" ''
     set -e
-    ${getExe diff-home-script} "$@"
+    ${getExe home-diff-script} "$@"
     function yes_or_no {
         while true; do
             read -p "$* [y/n]: " yn
@@ -80,10 +80,10 @@ let
     git commit -am "$(date -Iminutes)-home" || true
     ${getExe home-manager} switch --flake ".#$USER" "$@"
   '';
-  update-home-script = writeShellScriptBin "update-home" ''
+  home-update-script = writeShellScriptBin "home-update" ''
     git pull
     nix flake update
-    ${getExe switch-home-script} "$@"
+    ${getExe home-switch-script} "$@"
   '';
 in
 mkShellNoCC {
@@ -95,10 +95,10 @@ mkShellNoCC {
     update-script
     boot-script
 
-    build-home-script
-    diff-home-script
-    switch-home-script
-    update-home-script
+    home-build-script
+    home-diff-script
+    home-switch-script
+    home-update-script
 
     nvd
     nix-output-monitor
