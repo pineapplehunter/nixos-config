ba527224d2685187621d35a751486311f64c9297
Shogo Takata <peshogo@gmail.com>
2024-08-31 03:28:40 +0900

    2024-08-31T03:28+09:00

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -86,6 +86,12 @@
     type = "ibus";
   };
 
+  qt = {
+    enable = true;
+    platformTheme = "gnome";
+    style = "adwaita";
+  };
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
