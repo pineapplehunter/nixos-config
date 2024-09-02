d4e6b0a80be94e1a5effa38cedd7acb070f0aed1
Shogo Takata <peshogo@gmail.com>
2024-09-02 21:12:08 +0900

    2024-09-02T21:12+09:00

--- machines/action/configuration.nix
+++ machines/action/configuration.nix
@@ -138,26 +138,26 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  security.pam.services.login.fprintAuth = false;
-  security.pam.services.gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
-    text = ''
-      auth       required                    pam_shells.so
-      auth       requisite                   pam_nologin.so
-      auth       requisite                   pam_faillock.so      preauth
-      auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
-      auth       optional                    pam_permit.so
-      auth       required                    pam_env.so
-      auth       [success=ok default=1]      ${pkgs.gdm}/lib/security/pam_gdm.so
-      auth       optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so
+  # security.pam.services.login.fprintAuth = false;
+  # security.pam.services.gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
+  #   text = ''
+  #     auth       required                    pam_shells.so
+  #     auth       requisite                   pam_nologin.so
+  #     auth       requisite                   pam_faillock.so      preauth
+  #     auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+  #     auth       optional                    pam_permit.so
+  #     auth       required                    pam_env.so
+  #     auth       [success=ok default=1]      ${pkgs.gdm}/lib/security/pam_gdm.so
+  #     auth       optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so
 
-      account    include                     login
+  #     account    include                     login
 
-      password   required                    pam_deny.so
+  #     password   required                    pam_deny.so
 
-      session    include                     login
-      session    optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
-    '';
-  };
+  #     session    include                     login
+  #     session    optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+  #   '';
+  # };
 
   services.snapper.configs = {
     home = {
