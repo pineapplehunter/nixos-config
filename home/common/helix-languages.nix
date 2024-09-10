981d58c308bf9d7521f0f9d0415c461a7cb1458a
shogo <s196643z@st.go.tuat.ac.jp>
2024-09-10 16:18:31 +0900

    2024-09-10T16:18+09:00-home

--- home/common/helix-languages.nix
+++ home/common/helix-languages.nix
@@ -17,5 +17,20 @@
       name = "latex";
       soft-wrap.enable = true;
     }
+    {
+      name = "kconfig";
+      scope = "source.kconfig";
+      comment-token = "#";
+      file-types = [ { glob = "Kconfig"; } ];
+    }
+  ];
+  grammar = [
+    {
+      name = "kconfig";
+      source = {
+        git = "https://github.com/tree-sitter-grammars/tree-sitter-kconfig";
+        rev = "486fea71f61ad9f3fd4072a118402e97fe88d26c";
+      };
+    }
   ];
 }
