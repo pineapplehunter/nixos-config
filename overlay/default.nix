a0dff019541b829921993f146bf7d2ade80fcaea
Shogo Takata <peshogo@gmail.com>
2024-09-16 12:57:20 +0900

    remove blender overlay

--- overlay/default.nix
+++ overlay/default.nix
@@ -59,35 +59,6 @@ let
       gitify = final.callPackage (
         inputs.nixpkgs-pineapplehunter-gitify + /pkgs/by-name/gi/gitify/package.nix
       ) { };
-      # blender =mozc
-      #   let
-      #     python = final.python311Packages;
-      #     inherit (prev.blender) pname version;
-      #   in
-      #   final.symlinkJoin {
-      #     name = "${pname}-wrapped-${version}";
-      #     paths = [ prev.blender ];
-      #     nativeBuildInputs = [
-      #       final.makeWrapper
-      #       python.wrapPython
-      #     ];
-      #     pythonPath = with python; [
-      #       numpy
-      #       requests
-      #       py-slvs
-      #     ];
-      #     postBuild = ''
-      #       rm $out/bin/blender
-      #       mv $out/bin/.blender-wrapped $out/bin/blender
-
-      #       buildPythonPath "$pythonPath"
-      #       wrapProgram $out/bin/blender \
-      #         --prefix PATH : $program_PATH \
-      #         --prefix PYTHONPATH : "$program_PYTHONPATH" \
-      #         --add-flags "--python-use-system-env"
-      #     '';
-      #   };
-
       gnome = prev.gnome // {
         gnome-settings-daemon = prev.gnome.gnome-settings-daemon.overrideAttrs (old: {
           # I don't need sleep notifications!
