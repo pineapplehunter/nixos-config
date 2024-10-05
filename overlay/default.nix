fafdc4b87abee48b4318f562e4e727170cd7f9c5
Shogo Takata <takata.shogo@riken.jp>
2024-10-05 22:21:48 +0900

    update deps and add retry patch

--- overlay/default.nix
+++ overlay/default.nix
@@ -84,6 +84,9 @@ rec {
       version = builtins.head (builtins.match ''[^0-9]*([0-9\.]+).*'' old.version);
     });
     npm-lockfile-fix = inputs.npm-lockfile-fix.packages.${final.system}.default.overrideAttrs (old: {
+      patches = (old.patches or []) ++ [
+        ./retry.patch
+      ];
       meta = old.meta // {
         mainProgram = "npm-lockfile-fix";
       };
