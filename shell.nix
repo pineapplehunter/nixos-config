c4944e81f55d324452dc212630716e3b8cc0e6ee
Shogo Takata <peshogo@gmail.com>
2024-12-16 04:39:39 +0900

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -14,11 +14,12 @@ let
     ;
   inherit (pkgs.lib) getExe;
   os-build-script = writeShellScriptBin "os-build" ''
+    set -eou pipefail
     nom build ".#nixosConfigurations.$HOST.config.system.build.toplevel" "$@"
     exit $?
   '';
   os-diff-script = writeShellScriptBin "os-diff" ''
-    set -e
+    set -eou pipefail
     ${getExe os-build-script} "$@"
     if [ $(readlink -f ./result) = $(readlink -f /run/current-system) ]; then
       echo All packges up to date!
@@ -27,7 +28,7 @@ let
     nvd diff /run/current-system ./result
   '';
   os-switch-script = writeShellScriptBin "os-switch" ''
-    set -e
+    set -eou pipefail
     ${getExe os-diff-script} "$@"
     function yes_or_no {
         while true; do
@@ -44,23 +45,26 @@ let
     sudo nixos-rebuild switch --flake ".#$HOST" "$@"
   '';
   os-boot-script = writeShellScriptBin "os-boot" ''
-    set -e
+    set -eou pipefail
     ${getExe os-build-script} "$@"
     sudo echo switching boot
     sudo nixos-rebuild boot --flake ".#$HOST"
   '';
   os-update-script = writeShellScriptBin "os-update" ''
-    set -e
+    set -eou pipefail
     git pull
+    nix flake update nixpkgs
+    git diff --exit-code --quiet HEAD -- flake.lock && exit 1 || true
     nix flake update
     ${getExe os-switch-script} "$@" || git checkout HEAD -- flake.lock
   '';
   home-build-script = writeShellScriptBin "home-build" ''
+    set -eou pipefail
     nom build ".#homeConfigurations.$HOME_CONFIG_NAME.activationPackage" "$@"
     exit $?
   '';
   home-diff-script = writeShellScriptBin "home-diff" ''
-    set -e
+    set -eou pipefail
     ${getExe home-build-script} "$@"
     if [ $(readlink -f ./result) = $(readlink -f $HOME/.local/state/nix/profiles/home-manager) ]; then
       echo All packges up to date!
@@ -69,7 +73,7 @@ let
     nvd diff $HOME/.local/state/nix/profiles/home-manager ./result
   '';
   home-switch-script = writeShellScriptBin "home-switch" ''
-    set -e
+    set -eou pipefail
     ${getExe home-diff-script} "$@"
     function yes_or_no {
         while true; do
@@ -86,8 +90,10 @@ let
     home-manager switch -b "hm-backup" --flake ".#$HOME_CONFIG_NAME" "$@"
   '';
   home-update-script = writeShellScriptBin "home-update" ''
-    set -e
+    set -eou pipefail
     git pull
+    nix flake update nixpkgs
+    git diff --exit-code --quiet HEAD -- flake.lock && exit 1 || true
     nix flake update
     ${getExe home-switch-script} "$@" || git checkout HEAD -- flake.lock
   '';
