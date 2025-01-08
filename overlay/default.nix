fff7a792a8da3ef15e8e2ddb9921fb6edc1f65e6
Shogo Takata <shogo.takata@riken.jp>
2025-01-08 10:40:22 +0900

    remove flatpak patch

--- overlay/default.nix
+++ overlay/default.nix
@@ -46,15 +46,6 @@ rec {
       http3Support = true;
       openssl = prev.quictls;
     };
-    flatpak = prev.flatpak.overrideAttrs (old: {
-      postPatch =
-        (old.postPatch or "")
-        + ''
-          substituteInPlace common/flatpak-run.c \
-            --replace-fail "if (!sandboxed && !(flags & FLATPAK_RUN_FLAG_NO_DOCUMENTS_PORTAL))" "" \
-            --replace-fail "add_document_portal_args (bwrap, app_id, &doc_mount_path);" ""
-        '';
-    });
     android-studio = prev.android-studio.overrideAttrs {
       preferLocalBuild = true;
     };
