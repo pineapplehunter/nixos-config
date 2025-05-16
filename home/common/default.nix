491326a1bf307d3b1a91f49c8088e3bf5deb0310
Shogo Takata <peshogo@gmail.com>
2025-05-16 15:41:47 +0900

    add dconf settings to hm

--- home/common/default.nix
+++ home/common/default.nix
@@ -385,6 +385,26 @@ in
     end
   '';
 
+  dconf.settings = {
+    "org/gnome/desktop/applications/terminal" = {
+      exec = "ghostty";
+      exec-arg = "";
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Shift><Super>q" ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "Terminal";
+      command = "ghostty";
+      binding = "<Super>Return";
+    };
+  };
+
   home = {
     packages =
       let
