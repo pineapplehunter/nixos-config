cf89fd5b664ad6c1276d518e6f9033189d761337
Shogo Takata <peshogo@gmail.com>
2025-08-06 08:37:01 +0900

    hardware: fix action home volume path

--- machines/action/hardware-configuration.nix
+++ machines/action/hardware-configuration.nix
@@ -33,12 +33,12 @@
       device = "/dev/disk/by-uuid/c73fb028-c49b-4d3e-8628-39e326535d46";
       fsType = "btrfs";
       options = [
-        "subvol=@"
+        "autodefrag"
         "commit=120"
         "compress=zstd"
         "noatime"
         "space_cache=v2"
-        "autodefrag"
+        "subvol=@"
       ];
     };
 
@@ -46,12 +46,12 @@
       device = "/dev/disk/by-uuid/c73fb028-c49b-4d3e-8628-39e326535d46";
       fsType = "btrfs";
       options = [
-        "subvol=@"
+        "autodefrag"
         "commit=120"
         "compress=zstd"
         "noatime"
         "space_cache=v2"
-        "autodefrag"
+        "subvol=@home"
       ];
     };
 
