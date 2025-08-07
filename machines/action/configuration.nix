c2a24b7a02c98b54cdc617bb7da17038dcee643e
Shogo Takata <peshogo@gmail.com>
2025-08-07 09:13:16 +0900

    action: update pam configs

--- machines/action/configuration.nix
+++ machines/action/configuration.nix
@@ -238,131 +238,156 @@
 
   security.pam.services =
     let
-      default-rule =
-        let
-          inherit (pkgs)
-            fprintd
-            gnome-keyring
-            howdy
-            linux-pam
-            systemd
-            ;
-          # this directory will be wiped on every boot
-          stamp-dir = "/var/run/pam-timeout";
-          check-timeout = pkgs.writeShellScript "check-timeout.sh" ''
-            PATH=${lib.makeBinPath [ pkgs.coreutils ]}
-            # FIXME: this uses a plain text file that stores the time of
-            # last login.  Check if this is fine with my threat model.
-            # Hint for me: I assume no one except me has access to root user.
-            if [ -z "$PAM_USER" ]; then
-              echo no user is set
-              exit 1
-            fi
-            STAMP_FILE="${stamp-dir}/$PAM_USER/password_login_time"
-            MAX_AGE=$((12 * 60 * 60))  # 12 hours
+      inherit (pkgs)
+        fprintd
+        gnome-keyring
+        howdy
+        linux-pam
+        systemd
+        ;
+      # this directory will be wiped on every boot
+      stamp-dir = "/var/run/pam-timeout";
+      check-timeout = pkgs.writeShellScript "check-timeout.sh" ''
+        PATH=${lib.makeBinPath [ pkgs.coreutils ]}
+        # FIXME: this uses a plain text file that stores the time of
+        # last login.  Check if this is fine with my threat model.
+        # Hint for me: I assume no one except me has access to root user.
+        if [ -z "$PAM_USER" ]; then
+          echo no user is set
+          exit 1
+        fi
+        STAMP_FILE="${stamp-dir}/$PAM_USER/password_login_time"
+        MAX_AGE=$((12 * 60 * 60))  # 12 hours
 
-            if [[ ! -f "$STAMP_FILE" ]]; then
-              echo no stamp file found
-              exit 1  # Require password
-            fi
+        if [[ ! -f "$STAMP_FILE" ]]; then
+          echo no stamp file found
+          exit 1  # Require password
+        fi
 
-            last_time=$(< "$STAMP_FILE")
-            now=$(date +%s)
+        last_time=$(< "$STAMP_FILE")
+        now=$(date +%s)
 
-            if (( now - last_time < MAX_AGE )); then
-              exit 0  # OK to use fingerprint
-            else
-              exit 1  # Too old, force password
-            fi
-          '';
+        if (( now - last_time < MAX_AGE )); then
+          exit 0  # OK to use fingerprint
+        else
+          exit 1  # Too old, force password
+        fi
+      '';
 
-          update-timeout = pkgs.writeShellScript "update-timeout.sh" ''
-            PATH=${lib.makeBinPath [ pkgs.coreutils ]}
-            umask 077
-            if [ -z "$PAM_USER" ]; then
-              echo no user is set
-              exit 1
-            fi
-            STAMP_DIR="${stamp-dir}/$PAM_USER"
-            mkdir -p "$STAMP_DIR"
-            date +%s > "$STAMP_DIR/password_login_time"
-          '';
+      update-timeout = pkgs.writeShellScript "update-timeout.sh" ''
+        PATH=${lib.makeBinPath [ pkgs.coreutils ]}
+        umask 077
+        if [ -z "$PAM_USER" ]; then
+          echo no user is set
+          exit 1
+        fi
+        STAMP_DIR="${stamp-dir}/$PAM_USER"
+        mkdir -p "$STAMP_DIR"
+        date +%s > "$STAMP_DIR/password_login_time"
+      '';
 
-          # copied from nixpkgs
-          # https://github.com/NixOS/nixpkgs/blob/caf5a7d2d10c4cc33d02cf16f540ba79d6ccd004/nixos/modules/security/pam.nix#L1502-L1514
-          makeLimitsConf =
-            limits:
-            pkgs.writeText "limits.conf" (
-              lib.concatMapStrings (
-                {
-                  domain,
-                  type,
-                  item,
-                  value,
-                }:
-                "${domain} ${type} ${item} ${toString value}\n"
-              ) limits
-            );
-        in
-        ''
-          # Account management.
-          account required ${linux-pam}/lib/security/pam_unix.so
+      # copied from nixpkgs
+      # https://github.com/NixOS/nixpkgs/blob/caf5a7d2d10c4cc33d02cf16f540ba79d6ccd004/nixos/modules/security/pam.nix#L1502-L1514
+      makeLimitsConf =
+        limits:
+        pkgs.writeText "limits.conf" (
+          lib.concatMapStrings (
+            {
+              domain,
+              type,
+              item,
+              value,
+            }:
+            "${domain} ${type} ${item} ${toString value}\n"
+          ) limits
+        );
 
-          # Authentication management.
-          # This check introduces timeout on unix login.  It will only
-          # allow fingerprint or other means of login it unix has not
-          # timed out.  The control flow is created by skipping lines.
-          # It calls check-timeout twice to prevent toctou attacks
-          # !!! PLEASE CHECK SKIP LINES WHEN MODIFYING !!!
-          auth [success=ignore default=3] ${linux-pam}/lib/security/pam_exec.so quiet seteuid ${check-timeout}
-          auth [success=1 default=ignore] ${howdy}/lib/security/pam_howdy.so
-          auth [success=ignore default=ignore] ${fprintd}/lib/security/pam_fprintd.so
-          auth [success=4 default=ignore] ${linux-pam}/lib/security/pam_exec.so quiet seteuid ${check-timeout}
-          auth [success=1 default=ignore] ${linux-pam}/lib/security/pam_unix.so likeauth nullok try_first_pass
-          auth requisite ${linux-pam}/lib/security/pam_deny.so
-          auth optional ${linux-pam}/lib/security/pam_exec.so quiet seteuid ${update-timeout}
-          auth optional ${gnome-keyring}/lib/security/pam_gnome_keyring.so
-          auth requisite ${linux-pam}/lib/security/pam_permit.so
+      default-rule = ''
+        # Account management.
+        account required ${linux-pam}/lib/security/pam_unix.so
 
-          # Password management.
-          password sufficient ${linux-pam}/lib/security/pam_unix.so nullok yescrypt
-          password optional ${gnome-keyring}/lib/security/pam_gnome_keyring.so use_authtok
+        # Authentication management.
+        # This check introduces timeout on unix login.  It will only
+        # allow fingerprint or other means of login it unix has not
+        # timed out.  The control flow is created by skipping lines.
+        # It calls check-timeout twice to prevent toctou attacks
+        # !!! PLEASE CHECK SKIP LINES WHEN MODIFYING !!!
+        auth [success=ignore default=3]      ${linux-pam}/lib/security/pam_exec.so quiet seteuid ${check-timeout}
+        auth [success=1 default=ignore]      ${howdy}/lib/security/pam_howdy.so
+        auth [success=ignore default=ignore] ${fprintd}/lib/security/pam_fprintd.so
+        auth [success=4 default=ignore]      ${linux-pam}/lib/security/pam_exec.so quiet seteuid ${check-timeout}
+        auth [success=1 default=ignore]      ${linux-pam}/lib/security/pam_unix.so likeauth nullok try_first_pass
+        auth requisite                       ${linux-pam}/lib/security/pam_deny.so
+        auth optional                        ${linux-pam}/lib/security/pam_exec.so quiet seteuid ${update-timeout}
+        auth optional                        ${gnome-keyring}/lib/security/pam_gnome_keyring.so
+        auth requisite                       ${linux-pam}/lib/security/pam_permit.so
 
-          # Session management.
-          session required ${linux-pam}/lib/security/pam_env.so conffile=/etc/pam/environment readenv=0
-          session required ${linux-pam}/lib/security/pam_unix.so
-          session required ${linux-pam}/lib/security/pam_loginuid.so
-          session required ${linux-pam}/lib/security/pam_lastlog.so silent
-          session optional ${systemd}/lib/security/pam_systemd.so
-          session required ${linux-pam}/lib/security/pam_limits.so conf=${makeLimitsConf config.security.pam.services.login.limits}
-          session optional ${gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
-        '';
-      use-login-rule = ''
-        auth      substack      login
-        account   include       login
-        password  substack      login
-        session   include       login
+        # Password management.
+        password sufficient ${linux-pam}/lib/security/pam_unix.so nullok yescrypt
+        password optional   ${gnome-keyring}/lib/security/pam_gnome_keyring.so use_authtok
+
+        # Session management.
+        session required ${linux-pam}/lib/security/pam_env.so conffile=/etc/pam/environment readenv=0
+        session required ${linux-pam}/lib/security/pam_unix.so
+        session required ${linux-pam}/lib/security/pam_loginuid.so
+        session required ${linux-pam}/lib/security/pam_lastlog.so silent
+        session optional ${systemd}/lib/security/pam_systemd.so
+        session required ${linux-pam}/lib/security/pam_limits.so conf=${makeLimitsConf config.security.pam.services.login.limits}
+        session optional ${gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+      '';
+      only-unix-rule = ''
+        # Authentication management.
+        # This check introduces timeout on unix login.  It will only
+        # allow fingerprint or other means of login it unix has not
+        # timed out.  The control flow is created by skipping lines.
+        # It calls check-timeout twice to prevent toctou attacks
+        # !!! PLEASE CHECK SKIP LINES WHEN MODIFYING !!!
+        auth [success=1 default=ignore] ${linux-pam}/lib/security/pam_unix.so likeauth nullok try_first_pass
+        auth requisite                  ${linux-pam}/lib/security/pam_deny.so
+        auth optional                   ${linux-pam}/lib/security/pam_exec.so quiet seteuid ${update-timeout}
+        auth optional                   ${gnome-keyring}/lib/security/pam_gnome_keyring.so
+        auth requisite                  ${linux-pam}/lib/security/pam_permit.so
+
+        account  include default-auth
+        password include default-auth
+        sesstion include default-auth
+      '';
+      use-default-rule = ''
+        auth      substack      default-auth
+        account   include       default-auth
+        password  substack      default-auth
+        session   include       default-auth
+      '';
+      deny-all-rule = ''
+        auth      requisite ${linux-pam}/lib/security/pam_deny.so
+        account   requisite ${linux-pam}/lib/security/pam_deny.so
+        password  requisite ${linux-pam}/lib/security/pam_deny.so
+        session   requisite ${linux-pam}/lib/security/pam_deny.so
       '';
     in
     {
-      login.text = default-rule;
-      sudo.text = use-login-rule;
-      polkit-1.text = use-login-rule;
-      sudo-i.text = use-login-rule;
-      chfn.text = use-login-rule;
-      chpasswd.text = use-login-rule;
-      chsh.text = use-login-rule;
-      cups.text = use-login-rule;
-      groupadd.text = use-login-rule;
-      groupdel.text = use-login-rule;
-      groupmems.text = use-login-rule;
-      groupmod.text = use-login-rule;
-      passwd.text = use-login-rule;
-      systemd-run0.text = use-login-rule;
-      su.text = use-login-rule;
-      useradd.text = use-login-rule;
-      userdel.text = use-login-rule;
-      usermod.text = use-login-rule;
+      default-auth.text = default-rule;
+
+      chfn.text = only-unix-rule;
+      chpasswd.text = only-unix-rule;
+      chsh.text = only-unix-rule;
+      cups.text = use-default-rule;
+      gdm-fingerprint.text = lib.mkForce deny-all-rule;
+      gdm-password.text = use-default-rule;
+      groupadd.text = use-default-rule;
+      groupdel.text = only-unix-rule;
+      groupmems.text = use-default-rule;
+      groupmod.text = only-unix-rule;
+      login.text = use-default-rule;
+      passwd.text = only-unix-rule;
+      polkit-1.text = use-default-rule;
+      su.text = use-default-rule;
+      sudo-i.text = use-default-rule;
+      sudo.text = use-default-rule;
+      systemd-run0.text = use-default-rule;
+      useradd.text = use-default-rule;
+      userdel.text = only-unix-rule;
+      usermod.text = only-unix-rule;
     };
 
   programs.dconf = {
