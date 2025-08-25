977d2a3841eb53b5a95bff8683ed73c4f944f553
Shogo Takata <peshogo@gmail.com>
2025-08-25 11:21:59 +0900

    home.julia: auto update and auto add packages

--- home/julia/default.nix
+++ home/julia/default.nix
@@ -20,21 +20,49 @@
     home.packages = with pkgs; [ julia ];
 
     # precompile julia
-    systemd.user.services.julia-precompile = {
-      Unit.Description = "Precompile julia packages for fast repl";
-      Service = {
-        ExecStart = pkgs.writeShellScript "julia-precompile" ''
-          julia ${pkgs.writeText "julia-precompile.jl" ''
-            using Pkg
-            Pkg.precompile()
-          ''}
-        '';
-        Environment = [
-          "JULIA_DEPOT_PATH=${config.home.sessionVariables.JULIA_DEPOT_PATH}"
-          "PATH=${lib.makeBinPath [ pkgs.julia ]}"
-        ];
+    systemd.user = {
+      services = {
+        julia-precompile = {
+          Unit.Description = "Precompile julia packages for fast repl";
+          Service = {
+            ExecStart = pkgs.writeShellScript "julia-precompile" ''
+              julia ${pkgs.writeText "julia-precompile.jl" ''
+                using Pkg
+                ["OhMyREPL", "Unitful", "UnitfulData"] .|> Pkg.add
+                Pkg.precompile()
+              ''}
+            '';
+            Environment = [
+              "JULIA_DEPOT_PATH=${config.home.sessionVariables.JULIA_DEPOT_PATH}"
+              "PATH=${lib.makeBinPath [ pkgs.julia ]}"
+            ];
+          };
+          Install.WantedBy = [ "default.target" ];
+        };
+        julia-update = {
+          Unit.Description = "Update julia packages";
+          Service = {
+            ExecStart = pkgs.writeShellScript "julia-update" ''
+              julia ${pkgs.writeText "julia-update.jl" ''
+                using Pkg
+                Pkg.update()
+              ''}
+            '';
+            Environment = [
+              "JULIA_DEPOT_PATH=${config.home.sessionVariables.JULIA_DEPOT_PATH}"
+              "PATH=${lib.makeBinPath [ pkgs.julia ]}"
+            ];
+          };
+          Install.WantedBy = [ "default.target" ];
+        };
       };
-      Install.WantedBy = [ "default.target" ];
+      timers = {
+        julia-update = {
+          Unit.Description = "Update julia package";
+          Timer.OnCalendar = "weekly";
+          Install.WantedBy = [ "timers.target" ];
+        };
+      };
     };
   };
 }
