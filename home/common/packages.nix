37d514a7627c9883fa464927cb0e8be1e6b3f61a
Shogo Takata <peshogo@gmail.com>
2025-08-26 14:16:59 +0900

    home: add tpm2 and tss2 wrapper
    shell script wrapper for tpm2-tools

--- home/common/packages.nix
+++ home/common/packages.nix
@@ -1,4 +1,35 @@
 { pkgs, ... }:
+let
+  # multilib for bintools
+  multi-bintools = (pkgs.wrapBintoolsWith { bintools = pkgs.binutils-unwrapped-all-targets; });
+
+  tpm2-tools-wrapper =
+    name:
+    pkgs.writeShellScriptBin name ''
+      usage(){
+        echo Usage: $0 sub-command args
+        echo availible subcommands:
+        find ${pkgs.tpm2-tools}/bin -name "${name}_*" | xargs -I@ basename @ | sort | sed 's/${name}_/  /g'
+      }
+
+      if [ -z "$1" ]; then
+        echo sub command not specified
+        usage
+        exit 1
+      fi
+
+      subcmd="$1"
+      cmd="${pkgs.tpm2-tools}/bin/${name}_$subcmd"
+
+      if ! [ -f "$cmd" ]; then
+        echo sub command does not exist
+        usage
+        exit 2
+      fi
+      shift
+      exec "$cmd" "$@"
+    '';
+in
 {
   config.home.packages =
     with pkgs;
@@ -26,7 +57,8 @@
       zellij
     ]
     ++ [
-      # multilib for bintools
-      (pkgs.wrapBintoolsWith { bintools = pkgs.binutils-unwrapped-all-targets; })
+      (tpm2-tools-wrapper "tpm2")
+      (tpm2-tools-wrapper "tss2")
+      multi-bintools
     ];
 }
