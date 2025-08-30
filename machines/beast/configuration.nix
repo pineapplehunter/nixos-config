5f9bb4999c58d1fa298d59067b0abd97b26735ce
Shogo Takata <peshogo@gmail.com>
2025-08-30 11:44:06 +0900

    beast: change snapper settings

--- machines/beast/configuration.nix
+++ machines/beast/configuration.nix
@@ -92,11 +92,11 @@
         ALLOW_USERS = [ "shogo" ];
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
-        TIMELINE_LIMIT_HOURLY = 10;
-        TIMELINE_LIMIT_DAILY = 7;
-        TIMELINE_LIMIT_WEEKLY = 4;
-        TIMELINE_LIMIT_MONTHLY = 10;
-        TIMELINE_LIMIT_YEARLY = 2;
+        TIMELINE_LIMIT_HOURLY = 3;
+        TIMELINE_LIMIT_DAILY = 2;
+        TIMELINE_LIMIT_WEEKLY = 0;
+        TIMELINE_LIMIT_MONTHLY = 3;
+        TIMELINE_LIMIT_YEARLY = 0;
       };
 
       home = {
@@ -104,10 +104,10 @@
         ALLOW_USERS = [ "shogo" ];
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
-        TIMELINE_LIMIT_HOURLY = 10;
+        TIMELINE_LIMIT_HOURLY = 0;
         TIMELINE_LIMIT_DAILY = 7;
-        TIMELINE_LIMIT_WEEKLY = 4;
-        TIMELINE_LIMIT_MONTHLY = 10;
+        TIMELINE_LIMIT_WEEKLY = 2;
+        TIMELINE_LIMIT_MONTHLY = 6;
         TIMELINE_LIMIT_YEARLY = 2;
       };
     };
