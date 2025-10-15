f268dba749f7f3df9486fb885c8f8992b6345d23
Shogo Takata <shogo-takata@st.go.tuat.ac.jp>
2025-10-15 10:35:15 +0900

    modules/power-targets: use single service to manage states
    Enables external triggers such as resume from suspend.

--- modules/power-targets.nix
+++ modules/power-targets.nix
@@ -1,24 +1,38 @@
 {
-  flake.nixosModules.power-targets =
-    { config, ... }:
-    {
-      systemd.targets = {
+  flake.nixosModules.power-targets = {
+    systemd = {
+      targets = {
         power-ac.description = "Powered by AC";
         power-battery.description = "Powered by Battery";
       };
 
-      services.udev.extraRules = ''
-        # When AC adapter is plugged in
-        SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="1", ENV{POWER_SUPPLY_NAME}=="AC", \
-          RUN+="${config.systemd.package}/bin/systemctl --no-block start power-ac.target"
-        SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="1", ENV{POWER_SUPPLY_NAME}=="AC", \
-          RUN+="${config.systemd.package}/bin/systemctl --no-block stop power-battery.target"
-
-        # When AC adapter is unplugged
-        SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="0", ENV{POWER_SUPPLY_NAME}=="AC", \
-          RUN+="${config.systemd.package}/bin/systemctl --no-block stop power-ac.target"
-        SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="0", ENV{POWER_SUPPLY_NAME}=="AC", \
-          RUN+="${config.systemd.package}/bin/systemctl --no-block start power-battery.target"
-      '';
+      services = {
+        update-power-target = {
+          script = ''
+            POWER_ONLINE=$(cat /sys/class/power_supply/AC/online)
+            if [[ "$POWER_ONLINE" == "1" ]]; then
+              systemctl stop power-battery.target
+              systemctl start power-ac.target
+            else
+              systemctl stop power-ac.target
+              systemctl start power-battery.target
+            fi
+          '';
+          wantedBy = [ "default.target" ];
+        };
+        update-power-target-after-resume = {
+          script = ''
+            systemctl start update-power-target.service
+          '';
+          wantedBy = [ "suspend.target" ];
+          after = [ "suspend.target" ];
+        };
+      };
     };
+
+    services.udev.extraRules = ''
+      # Update power target on power state change
+      SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_NAME}=="AC", TAG+="systemd", ENV{SYSTEMD_WANTS}="update-power-target.service"
+    '';
+  };
 }
