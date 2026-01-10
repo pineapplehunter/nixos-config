bed8678c5a276c523e2b10ab09609fce803f708e
Shogo Takata <shogo-takata@st.go.tuat.ac.jp>
2026-01-10 20:40:03 +0900

    rpi5: fmt

--- modules/machines/rpi5/pi5-configtxt.nix
+++ modules/machines/rpi5/pi5-configtxt.nix
@@ -1,20 +1,26 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   hardware.raspberry-pi.config = {
-    all = { # [all] conditional filter, https://www.raspberrypi.com/documentation/computers/config_txt.html#conditional-filters
+    all = {
+      # [all] conditional filter, https://www.raspberrypi.com/documentation/computers/config_txt.html#conditional-filters
 
       options = {
         # https://www.raspberrypi.com/documentation/computers/config_txt.html#enable_uart
         # in conjunction with `console=serial0,115200` in kernel command line (`cmdline.txt`)
-        # creates a serial console, accessible using GPIOs 14 and 15 (pins 
+        # creates a serial console, accessible using GPIOs 14 and 15 (pins
         #  8 and 10 on the 40-pin header)
         enable_uart = {
           enable = true;
           value = true;
         };
         # https://www.raspberrypi.com/documentation/computers/config_txt.html#uart_2ndstage
-        # enable debug logging to the UART, also automatically enables 
+        # enable debug logging to the UART, also automatically enables
         # UART logging in `start.elf`
         uart_2ndstage = {
           enable = true;
