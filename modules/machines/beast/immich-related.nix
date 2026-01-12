43ad8ec9977ae79b26be9da4171d3405a82fa3d7
Shogo Takata <peshogo@gmail.com>
2026-01-13 02:34:41 +0900

    beast: remove geesefs

--- modules/machines/beast/immich-related.nix
+++ modules/machines/beast/immich-related.nix
@@ -52,51 +52,14 @@ in
 
       # to startup immich at boot
       systemd.services = {
-        geesefs-mount = {
-          restartIfChanged = false;
-          path = with pkgs; [
-            fuse
-            geesefs
-            util-linux
-          ];
-          requires = [ "garage.service" ];
-          after = [ "garage.service" ];
-          script = ''
-            findmnt mnt && umount mnt || true
-            geesefs \
-              --endpoint http://localhost:3900 \
-              --region garage \
-              --list-type 2 \
-              --memory-limit ${toString (1024 * 4)} \
-              --stat-cache-ttl 10m \
-              --cache mnt-cache \
-              --http-timeout 0 \
-              --print-stats 5m \
-              --shared-config ${config.age.secrets.geesefs-creds.path} \
-              -o allow_other \
-              immich: \
-              mnt
-            ls mnt
-          '';
-          serviceConfig = {
-            Type = "forking";
-            WorkingDirectory = "/home/shogo/immich";
-            TimeoutSec = 600;
-            Restart = "always";
-          };
-        };
         immich-up = {
           enable = true;
           restartIfChanged = false;
           path = with pkgs; [
-            curl
             docker
             docker-compose
           ];
-          requires = [
-            "geesefs-mount.service"
-            "docker.socket"
-          ];
+          requires = [ "docker.socket" ];
           after = [
             "geesefs-mount.service"
             "docker.socket"
