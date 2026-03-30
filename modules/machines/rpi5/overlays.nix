bdd66c6e7faefbcfb779df9cf1842757df311076
Shogo Takata <shogo-takata@st.go.tuat.ac.jp>
2026-03-30 10:28:28 +0900

    rpi5: fix ffmpeg with overlay

--- modules/machines/rpi5/overlays.nix
+++ modules/machines/rpi5/overlays.nix
@@ -8,4 +8,15 @@ final: prev: {
       hash = "sha256-ZSKNeFDedqzcVxoLPap2dMjq+F3C1eQ+HikEKuGBOyM=";
     };
   });
+
+  ffmpeg_8 = prev.ffmpeg_8.overrideAttrs rec {
+    version = "8.1";
+    patches = [ ];
+    src = final.fetchFromGitHub {
+      owner = "jc-kynesim";
+      repo = "rpi-ffmpeg";
+      rev = "n${version}";
+      hash = "sha256-FdKhhCveEo5UodEoyUh3aBHABv3OT2VXmwBXE1ce3p0=";
+    };
+  };
 }
