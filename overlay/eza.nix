4e30be9962fd6c4cdcd7d1e2ba4bbd968ee9f8e7
Shogo Takata <shogo-takata@st.go.tuat.ac.jp>
2026-08-06 12:02:46 +0900

    overlay/eza: update branch

--- overlay/eza.nix
+++ overlay/eza.nix
@@ -3,19 +3,19 @@
     # Add capability support (security.capability xattr display).
     # This can be dropped when the PR is merged and included in the nixpkgs package.
     # https://github.com/eza-community/eza/pull/1624 (still open)
-    # Last checked: 2026-08-02.
+    # Last checked: 2026-08-06.
     eza = prev.eza.overrideAttrs (
       finalAttrs: prevAttrs: {
         version = "0-custom";
         src = final.fetchFromGitHub {
           owner = "pineapplehunter";
           repo = "eza";
-          rev = "39ae9d32d8936e539c2f4ca0042fc31fcf0068a1";
-          hash = "sha256-OEgql1Wj79EkoGZ/ZgmFVwMmCgLIhukqehCs/Gg7dLA=";
+          rev = "a5b4beaf75ca2707cd5bfd0daa52a40819d956e2";
+          hash = "sha256-xygdTXbWB8rmurZR6r+peJOvaGlzgx9tgFwCWwXH7DU=";
         };
         cargoDeps = final.rustPlatform.fetchCargoVendor {
           inherit (finalAttrs) src pname version;
-          hash = "sha256-J6Qu8FFlp3PMTm0M/XT4TqQPaqH57TLBPhQE1Y5hdjg=";
+          hash = "sha256-RBHeP73ivtbEFl0Q1tXdSLmkteiaaYOiNSD6M+4ZUdA=";
         };
         doInstallCheck = false;
       }
